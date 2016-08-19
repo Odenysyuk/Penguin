e38a680a47574ec036db9b7444b76286b72982cb
Odenysyuk <ovdenysyuk@gmail.com>
2016-08-20 01:03:50 +0300

    Add class User

---
+++ Penguin/Penguin/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client;
+
+namespace Penguin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User Client = new User();
+            Client.Name = "Olena";
+            Console.WriteLine(Client.Name);
+        }
+    }
+}
+
+namespace Client
+{
+    class User
+    {
+        public string Name
+        {
+            set;
+            get;
+        }
+
+        public string Surname
+        {
+            set;
+            get;
+        }
+
+        public string Login
+        {
+            set;
+            get;
+        }
+
+        public string Password
+        {
+            set;
+            get;
+        }
+
+        public uint AmountWordsOfDay
+        {
+            set;
+            get;
+        }
+
+        public DateTime DateOfBorned
+        {
+            set;
+            get;
+        }
+
+        public Dictionary<string, string> LearnedWords;
+
+        public void UpdateUser()
+        {
+            string choice = "-1";
+
+            Console.WriteLine("\t Menu:");
+            Console.WriteLine("1. Change name (Enter -1);");
+            Console.WriteLine("2. Change surname (Enter -2);");
+            Console.WriteLine("3. Change login (Enter -3);");
+            Console.WriteLine("4. Change password (Enter -4);");
+            Console.WriteLine("5. Change amount of word to learn (Enter -5);");
+            Console.WriteLine("6. Change date of birthday (Enter - 6);");
+            Console.WriteLine("7. Change clean learning words(Enter - 7);");
+            Console.WriteLine("0. Exit (Enter - 0);");
+            choice = Console.ReadLine();
+
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter new name:");
+                    Name = Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("Enter surname name:");
+                    Surname = Console.ReadLine();
+                    break;
+                case "3":
+                    Console.WriteLine("Enter new login:");
+                    Login = Console.ReadLine();
+                    break;
+                case "4":
+                    Console.WriteLine("Enter new password:");
+                    Password = Console.ReadLine();
+                    break;
+                case "5":
+                    Console.WriteLine("Enter new amount of words to learn:");
+
+                    try
+                    {
+                        AmountWordsOfDay = UInt32.Parse(Console.ReadLine());
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Incorrect choice. Please, try again!!!");
+                        UpdateUser();
+                    }        
+                    break;
+                case "6":
+                    Console.WriteLine("Enter new date of birthday:");
+
+                    try
+                    {
+                        DateOfBorned = DateTime.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Incorrect choice. Please, try again!!!");
+                        UpdateUser();
+                    }
+                    break;
+                case "7":
+                    LearnedWords.Clear();
+                    break;
+                case "0":
+                    Console.WriteLine("Bye-bye!!!");
+                    break;
+                default:
+                    Console.WriteLine("Incorrect choice. Please, try again!!!");
+                    UpdateUser();
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} ({2})", Name, Surname, Login);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Penguin
 Penguin is my first project by studying and repeating English words
 
-In progress
+#In progress
 
-#What is doing?
+#Plan:
 Program works with mongodb.
 Parsing xdxf document into mongodb.
 CRUD in MongoDB.
 Serialization and deserialization BSON. 
+Use website http://www.dictionary.com/browse/say  listen to words
 
 #Purpose
 Purpose is language learning in practice.
 Develop program to learn English everyday. There're users, English_Ukraine vocabulary,   setting of users, statistics studied words.
 
-
+#What is doing?
+1. Parse xdxf in program;
+2. 
